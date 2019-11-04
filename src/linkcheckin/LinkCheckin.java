9b7465cf241de94a703c5a3512fcb8e1e2364443
Daniloctg <daniloctg@msn.com>
2019-11-04 08:07:53 -0200

    Commit inicial do projeto

---
+++ src/linkcheckin/LinkCheckin.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkcheckin;
+
+/**
+ *
+ * @author Administração
+ */
+public class LinkCheckin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu a = new Menu();
+        a.setVisible(true);
+    }
+    
+}
